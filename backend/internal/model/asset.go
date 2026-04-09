12ab3f796803c0a315f39fd3a8da4542d80c7d0d
Whatevahappens <osohitooss@gmail.com>
2026-04-10 00:04:19 +0800

    add asset model

--- backend/internal/model/asset.go
+++ backend/internal/model/asset.go
@@ -1,1 +1,17 @@
 package model
+
+import "time"
+
+type Asset struct {
+	ID               string
+	Barcode          string
+	SerialNumber     string
+	AssetName        string
+	AcquisitionPrice int
+	AcquisitionDate  time.Time
+	UsefulLifeMonths int
+	CurrentValue     int
+	Status           AssetStatus
+	Nature           AssetNature
+	Description      string
+}
