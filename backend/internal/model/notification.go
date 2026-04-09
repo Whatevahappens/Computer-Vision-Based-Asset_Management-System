be7914e23bffa7b71420d0dd53103090c66f9eeb
Whatevahappens <osohitooss@gmail.com>
2026-04-09 22:10:06 +0800

    add notification model

--- backend/internal/model/notification.go
+++ backend/internal/model/notification.go
@@ -1,1 +1,12 @@
 package model
+
+import "time"
+
+type Notification struct {
+	ID        string
+	Title     string
+	Message   string
+	Type      NotificationType
+	IsRead    bool
+	CreatedAt time.Time
+}
